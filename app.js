a11206ccd13c40a34dc676ad9e1c84a147368f6d
Vignesh-Vicky-Vicky <85555447+Vignesh-Vicky-Vicky@users.noreply.github.com>
2021-06-12 17:20:17 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,47 +6,31 @@ let reset = document.getElementById("reset");
 let output = document.getElementById("output");
 
 
-var counter = 0;
+let count = 0;
 
-button.addEventListener('click', function () {
+button.addEventListener('click', () => {
 
-    var input = String(inputvalue.value);
+    let input = inputvalue.value;
+    let vowels = 'aeiouAEIOU';
 
     for (let i = 0; i < input.length; i++) {
 
-        var value = input[i];
+        let wordcheck = input[i];
 
-        switch (value) {
-            case "a": counter++;
-                break;
-            case "e": counter++;
-                break;
-            case "i": counter++;
-                break;
-            case "o": counter++;
-                break;
-            case "u": counter++;
-                break;
-            case "A": counter++;
-                break;
-            case "E": counter++;
-                break;
-            case "I": counter++;
-                break;
-            case "O": counter++;
-                break;
-            case "U": counter++;
-                break;
-        }
+            for(let i = 0; i<vowels.length; i++){
+                if(wordcheck == vowels[i]){
+                    count++;
+                }
+            }
 
     }
-    output.innerHTML = counter;
-    inputvalue.value = "";
+    output.innerHTML = count;
 
 })
 
-reset.addEventListener('click', function () {
-    counter = 0;
-    output.innerHTML = counter;
-    inputvalue.value = "";
+reset.addEventListener('click', () =>{
+    count = 0;
+    inputvalue.value= '';
+    output.innerHTML = count;
 })
+
