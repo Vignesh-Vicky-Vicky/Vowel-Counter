9b8d684c2efebcf0d2f78e7016b15d3effd056ce
vignesh <karthikvignesh3@gmail.com>
2022-07-23 22:14:55 +0530

    added conditions to check input value

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-
-
 let inputvalue = document.getElementById("inputvalue");
 let button = document.getElementById("button");
 let reset = document.getElementById("reset");
 let output = document.getElementById("output");
 
-
 let count = 0;
 
-button.addEventListener('click', () => {
-    
-    let input = inputvalue.value;
-    let vowels = 'aeiouAEIOU';
+inputvalue.addEventListener("input", () => {
+  if (inputvalue.value.trim()) {
+    button.classList.remove("disabled");
+    reset.classList.remove("disabled");
+  } else {
+    button.classList.add("disabled");
+    reset.classList.add("disabled");
+  }
+});
 
+button.addEventListener("click", () => {
+  count = 0;
+  let input = inputvalue.value.trim();
+  let vowels = "aeiouAEIOU";
+
+  if (input) {
     for (let i = 0; i < input.length; i++) {
+      let wordcheck = input[i];
 
-        let wordcheck = input[i];
-
-            for(let i = 0; i<vowels.length; i++){
-                if(wordcheck == vowels[i]){
-                    count++;
-                }
-            }
-
+      for (let i = 0; i < vowels.length; i++) {
+        if (wordcheck == vowels[i]) {
+          count++;
+        }
+      }
     }
     output.innerHTML = count;
+  }
+});
 
-})
-
-reset.addEventListener('click', () =>{
-    count = 0;
-    inputvalue.value= '';
-    output.innerHTML = count;
-})
-
+reset.addEventListener("click", () => {
+  count = 0;
+  inputvalue.value = "";
+  output.innerHTML = "Enter something to check";
+  button.classList.add("disabled");
+  reset.classList.add("disabled");
+});
